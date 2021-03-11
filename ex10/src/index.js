d20cebd26cb23a8657dbc18215a21fab3d4c1f4f
Krsto Kostic <kostic.krsto@gmail.com>
2021-03-12 00:38:01 +0100

    Adding ex for day04

---
+++ ex10/src/index.js
@@ -0,0 +1,15 @@
+function main() {
+    // Only change code below this line
+    var myFirstName = "Krsto";
+    var myLastName = "Kostić";
+    var myHomeTown = "Nevesinje";
+    // Only change code above this line
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+
+console.log(main());
+module.exports = main;
